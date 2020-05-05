8b683537c5eccf1e9562265cb5cdc9097d4035a1
Zeng <1933920351@qq.com>
2020-05-05 18:24:03 +0800

    http工具优化

--- src/main/java/utils/http/HttpTools.java
+++ src/main/java/utils/http/HttpTools.java
@@ -2,9 +2,7 @@ package utils.http;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -12,219 +10,164 @@ import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.*;
 
 
 public class HttpTools {
-//    static String baseUrl = "http://127.0.0.1:8086/";
-    static String baseUrl = "http://api.zeml.top:8086/";
-    static CookieStore cookieStore = null;  // 保持cookie
+    // 设置配置请求参数
+//    private static String baseUrl = "http://127.0.0.1:8086/";
+    private static String baseUrl = "http://api.zeml.top:8086/";
+    private static RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)    // 连接主机服务超时时间
+            .setConnectionRequestTimeout(60000) // 请求超时时间
+            .setSocketTimeout(120000)   // 数据读取超时时间
+            .build();
 
-    /**
-     * 获取客户端的ip
-     * //     * @param request
-     *
-     * @return
-     */
-    public static String getRemortIP(HttpServletRequest request) {
-        String ipAddress = null;
-        try {
-            ipAddress = request.getHeader("x-forwarded-for");
-            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-                ipAddress = request.getHeader("Proxy-Client-IP");
+
+    private static CookieStore cookieStore = null;  // 保持cookie
+
+    private static MyResponse doCookieRequest(HttpUriRequest request){
+        MyResponse response = null;
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse httpResponse = null;
+
+        // 创建HttpClientBuilder
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        // 如果没有cookie，先获取cookie
+        if(cookieStore == null){
+            try {
+                CloseableHttpClient httpClient2 = httpClientBuilder.build();
+                CloseableHttpResponse httpResponse2 = httpClient2.execute(request);
+                setCookieStore(httpResponse2);
+            }catch (Exception e){
+                e.printStackTrace();
             }
-            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-                ipAddress = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-                ipAddress = request.getRemoteAddr();
-                if (ipAddress.equals("127.0.0.1")) {
-                    // 根据网卡取本机配置的IP
-                    InetAddress inet = null;
+        }
+
+        if(cookieStore != null) {
+            try {
+                httpClient = httpClientBuilder.setDefaultCookieStore(cookieStore).build();
+                httpResponse = httpClient.execute(request);
+                // 通过返回对象获取返回数据
+                HttpEntity entity = httpResponse.getEntity();
+                // 通过EntityUtils中的toString方法将结果转换为字符串
+                String result = EntityUtils.toString(entity);
+
+                // 解析字符串
+                if (result != null && !result.equals("")) {
+                    response = JSON.parseObject(result, MyResponse.class);
+                } else {
+                    response = MyResponse.failureResponse("网络连接异常！");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                // 释放资源
+                if (httpClient != null) {
                     try {
-                        inet = InetAddress.getLocalHost();
-                    } catch (UnknownHostException e) {
+                        httpClient.close();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    ipAddress = inet.getHostAddress();
+                }
+                if (httpResponse != null) {
+                    try {
+                        httpResponse.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
-                // = 15
-                if (ipAddress.indexOf(",") > 0) {
-                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-                }
-            }
-        } catch (Exception e) {
-            ipAddress = "";
-        }
-        // ipAddress = this.getRequest().getRemoteAddr();
-        return ipAddress;
-    }
-
-
-
-    /**  Get请求  **/
-    public static MyResponse doGet(String url) {
-        url = baseUrl + url;
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        String result = "";
-        try {
-            // 通过址默认配置创建一个httpClient实例
-            httpClient = HttpClients.createDefault();
-            // 创建httpGet远程连接实例
-            HttpGet httpGet = new HttpGet(url);
-            // 设置请求头信息，鉴权
-            httpGet.setHeader("Authorization", "Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0");
-            // 设置配置请求参数
-            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)// 连接主机服务超时时间
-                    .setConnectionRequestTimeout(60000)// 请求超时时间
-                    .setSocketTimeout(120000)// 数据读取超时时间
-                    .build();
-            // 为httpGet实例设置配置
-            httpGet.setConfig(requestConfig);
-            // 执行get请求得到返回对象
-            response = httpClient.execute(httpGet);
-            // 通过返回对象获取返回数据
-            HttpEntity entity = response.getEntity();
-            // 通过EntityUtils中的toString方法将结果转换为字符串
-            result = EntityUtils.toString(entity);
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            // 关闭资源
-            if (null != response) {
-                try {
-                    response.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        }else {
+            response = MyResponse.errorResponse("未获取到cookie！");
         }
 
-        if(result != null && !result.equals("")){
-            return JSON.parseObject(result, MyResponse.class);
-        }else return null;
+        return response;
+
     }
 
-    public static void setCookieStore() {
-        String url = baseUrl + "userAccount/test";
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
+    public static void setCookieStore(CloseableHttpResponse httpResponse) {
         try {
-            httpClient = HttpClients.createDefault();
-            HttpGet httpGet = new HttpGet(url);
-            // 设置配置请求参数
-            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)// 连接主机服务超时时间
-                    .setConnectionRequestTimeout(60000)// 请求超时时间
-                    .setSocketTimeout(120000)// 数据读取超时时间
-                    .build();
-            // 为httpGet实例设置配置
-            httpGet.setConfig(requestConfig);
-            // 执行get请求得到返回对象
-            response = httpClient.execute(httpGet);
-            // 通过返回对象获取返回数据
-
-
             System.out.println("----setCookieStore");
             cookieStore = new BasicCookieStore();
             // JSESSIONID
-            String setCookie = response.getFirstHeader("Set-Cookie").getValue();
+            String setCookie = httpResponse.getFirstHeader("Set-Cookie").getValue();
             String JSESSIONID = setCookie.substring("JSESSIONID=".length(),
                     setCookie.indexOf(";"));
             System.out.println("JSESSIONID:" + JSESSIONID);
             // 新建一个Cookie
             BasicClientCookie cookie = new BasicClientCookie("JSESSIONID",JSESSIONID);
             cookie.setVersion(0);
-//        cookie.setDomain("domain");
-//        cookie.setPath("/xxx");
             cookieStore.addCookie(cookie);
             System.out.println("cookie设置成功！");
-        } catch (ClientProtocolException e) {
+        }catch (Exception e){
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            // 关闭资源
-            if (null != response) {
-                try {
-                    response.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        }
+    }
+
+
+    /**  Get请求 无参数  **/
+    public static  MyResponse doGet(String url){
+        return doGet(url, null);
+    }
+
+    /**  Get请求  **/
+    public static  MyResponse doGet(String url, Map<String, String> paramMap) {
+        MyResponse response = null;
+
+        // 把数据加到url中
+        if(paramMap != null && paramMap.size() > 0){
+            Set<Map.Entry<String, String>> entrySet = paramMap.entrySet();
+            Iterator<Map.Entry<String, String>> iterator = entrySet.iterator();
+            int i=0;
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> mapEntry = iterator.next();
+                url += (i == 0)?"?":"&";
+                url += mapEntry.getKey();
+                url += "=";
+                url += mapEntry.getValue();
+                i++;
             }
         }
 
+        // 数据输出
+        System.out.println(url);
 
+        try{
+            // 创建httpGet远程连接实例
+            HttpGet httpGet = new HttpGet(baseUrl + url);
+            // 为httpGet实例设置配置
+            httpGet.setConfig(requestConfig);
+
+            response = doCookieRequest(httpGet);
+        }catch (Exception e){
+            e.printStackTrace();
+            response = MyResponse.errorResponse("Get请求异常！");
+        }
+
+        return  response;
     }
+
 
     /**  Post请求  **/
     public static MyResponse doPost(String url, Map<String, String> paramMap) {
-        url = baseUrl + url;
+        MyResponse response = null;
+        UrlEncodedFormEntity data = null;
+
+        // 数据输出
         System.out.println(url);
         if(paramMap != null){
             System.out.println(paramMap.toString());
         }
 
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse httpResponse = null;
-        String result = "";
-
-        // 创建HttpClientBuilder
-        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-        if(cookieStore == null){
-            setCookieStore();
-        }
-
-        if(cookieStore!=null){
-            httpClient = httpClientBuilder.setDefaultCookieStore(cookieStore).build();
-        }else{
-            return MyResponse.failureResponse("cookie获取失败！");
-        }
-
-//        // 创建httpClient实例
-//        httpClient = HttpClients.createDefault();
-        // 创建httpPost远程连接实例
-        HttpPost httpPost = new HttpPost(url);
-        // 配置请求参数实例
-        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)// 设置连接主机服务超时时间
-                .setConnectionRequestTimeout(60000)// 设置连接请求超时时间
-                .setSocketTimeout(120000)// 设置读取数据连接超时时间
-                .build();
-        // 为httpPost实例设置配置
-        httpPost.setConfig(requestConfig);
-        // 设置请求头
-        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        // 封装post请求参数
-        if (null != paramMap && paramMap.size() > 0) {
+        // 数据封装
+        if (paramMap != null && paramMap.size() > 0) {
             List<NameValuePair> nvps = new ArrayList<NameValuePair>();
             // 通过map集成entrySet方法获取entity
             Set<Map.Entry<String, String>> entrySet = paramMap.entrySet();
@@ -234,95 +177,31 @@ public class HttpTools {
                 Map.Entry<String, String> mapEntry = iterator.next();
                 nvps.add(new BasicNameValuePair(mapEntry.getKey(), mapEntry.getValue()));
             }
-
-            // 为httpPost设置封装好的请求参数
             try {
-                httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+                data = new UrlEncodedFormEntity(nvps, "UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
         }
-        try {
-            // httpClient对象执行post请求,并返回响应参数对象
-            httpResponse = httpClient.execute(httpPost);
-            // 从响应对象中获取响应内容
-            HttpEntity entity = httpResponse.getEntity();
-            result = EntityUtils.toString(entity);
-        } catch (ClientProtocolException e) {
+
+        try{
+            // 创建httpPost远程连接实例
+            HttpPost httpPost = new HttpPost(baseUrl + url);
+            // 配置请求参数实例
+
+            // 为httpPost实例设置配置
+            httpPost.setConfig(requestConfig);
+            // 设置请求头
+            httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            // 封装post请求参数
+            if (data != null) {
+                httpPost.setEntity(data);
+            }
+            response = doCookieRequest(httpPost);
+        }catch (Exception e){
+            response = MyResponse.errorResponse("Post请求异常！");
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            // 关闭资源
-            if (null != httpResponse) {
-                try {
-                    httpResponse.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        System.out.println("post请求返回的结果：" + result);
-        if(result != null && !result.equals("")){
-            return JSON.parseObject(result, MyResponse.class);
-        }else return null;
-    }
-
-
-    /**
-     * 将字符串的编码格式转换为utf-8
-     *
-     * @param str
-     * @return Name = new
-     * String(Name.getBytes("ISO-8859-1"), "utf-8");
-     */
-    public static String toUTF8(String str) {
-        if (isEmpty(str)) {
-            return "";
-        }
-        try {
-            if (str.equals(new String(str.getBytes("GB2312"), "GB2312"))) {
-                str = new String(str.getBytes("GB2312"), "utf-8");
-                return str;
-            }
-        } catch (Exception exception) {
-        }
-        try {
-            if (str.equals(new String(str.getBytes("ISO-8859-1"), "ISO-8859-1"))) {
-                str = new String(str.getBytes("ISO-8859-1"), "utf-8");
-                return str;
-            }
-        } catch (Exception exception1) {
-        }
-        try {
-            if (str.equals(new String(str.getBytes("GBK"), "GBK"))) {
-                str = new String(str.getBytes("GBK"), "utf-8");
-                return str;
-            }
-        } catch (Exception exception3) {
-        }
-        return str;
-    }
-
-
-    /**
-     * 判断是否为空
-     *
-     * @param str
-     * @return
-     */
-    public static boolean isEmpty(String str) {
-        // 如果字符串不为null，去除空格后值不与空字符串相等的话，证明字符串有实质性的内容
-        if (str != null && !str.trim().isEmpty()) {
-            return false;// 不为空
-        }
-        return true;// 为空
+        return response;
     }
 }
