b1c4c67be30bdc403fcf0ec5b01e3f404d7be5b9
gloria_wt <736909323@qq.com>
2020-06-16 17:58:36 +0800

    数学绘图模式-矩形

---
+++ src/main/java/controller/drawMath/RectangleController.java
@@ -0,0 +1,33 @@
+package controller.drawMath;
+
+import entity.ShapeRectangle;
+import entity.ShapeTriangle;
+import view.drawMath.DrawRectangle;
+
+public class RectangleController implements  DrawRectangle.Listener{
+    DrawRectangle drawRectangle;
+    ShapeRectangle shapeRectangle;
+
+    public RectangleController(DrawRectangle drawRectangle){this.drawRectangle=drawRectangle;}
+
+    @Override
+    public void onDraw1(int x, int y, int length, int width) {
+      shapeRectangle=new ShapeRectangle();
+      shapeRectangle.setX(x);
+      shapeRectangle.setY(y);
+      shapeRectangle.setWidth(width);
+      shapeRectangle.setLength(length);
+      calculateInfo();
+      drawRectangle.drawShape(shapeRectangle);
+      System.out.println(shapeRectangle.toString());
+      drawRectangle.updateInfoArea(shapeRectangle);
+    }
+    private void calculateInfo(){
+        //计算周长
+        shapeRectangle.setPerimeter(2*(shapeRectangle.getWidth()+shapeRectangle.getLength()));
+        //计算面积
+        shapeRectangle.setArea(shapeRectangle.getLength()+shapeRectangle.getWidth());
+    }
+
+
+}
